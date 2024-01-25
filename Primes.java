bbc9b0e1a5afd6552dc01e93d5c9323cfd779009
Anna Local <anna.macinnis109@gmail.com>
2024-01-25 10:57:40 -0500

    Removed sequential testing

--- Primes.java
+++ Primes.java
@@ -1,23 +1,18 @@
 // todo author and problem statement comment
 // Finds and prints all primes on an input-defined range.
 
-import java.lang.Math;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.TreeSet;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Primes
 {
     private static final int NUM_THREADS = 8;
+    // Amount of maximum primes to track.
     private static final int AMT_LARGEST = 10;
 
     static Scanner in = new Scanner(System.in);
@@ -40,15 +35,13 @@ public class Primes
     }
 
     // Updates the set storing the 10 maximum primes.
+    // Any n passed in will be larger than the minimum number in the set?? (todo)
     private synchronized static void updateMaxes(TreeSet<Integer> maxes, int n)
     {
-        if (maxes.size() < AMT_LARGEST)
-            maxes.add(n);
-        else if (maxes.first() < n)
-        {
+        if (maxes.size() == AMT_LARGEST)
             maxes.pollFirst();
-            maxes.add(n);
-        }
+
+        maxes.add(n);
     }
 
     public static void main(String [] args)
@@ -56,26 +49,10 @@ public class Primes
         // Upper limit input validation.
         int upLim = (int) Math.floor(in.nextDouble());
 
-        long startSeq = System.nanoTime();
-        int numPrimesSeq = 0;
-        long sumPrimesSeq = 0;
-
-        for (int i = 0; i < upLim; i++)
-        {
-            if (isPrime(i))
-            {
-                numPrimesSeq++;
-                sumPrimesSeq += i;
-            }
-        }
-        long endSeq = System.nanoTime();
-
-        System.out.println((endSeq - startSeq) + " " + numPrimesSeq + " " + sumPrimesSeq);
-
         // Threaded approach.
         long start = System.nanoTime();
 
-        // Take care of edge case.
+        // Take care of edge case. (todo)
         if (upLim < 2)
             System.out.println("There are no primes less than 2!");
 
@@ -85,15 +62,15 @@ public class Primes
         TreeSet<Integer> maxPrimes = new TreeSet<>();
         maxPrimes.add(2); // We know the only even prime is in the range.
 
-        // List of tasks for threads to do.
+        // Initialize list of tasks for threads to do.
         ArrayList<FutureTask<PrimesInfo>> tasks = new ArrayList<FutureTask<PrimesInfo>>();
         for (int i = 0; i < NUM_THREADS; i++)
         {
             tasks.add(new FutureTask<>(new Callable<PrimesInfo>() {
 
-                // Maintains thread-specific numPrimes and sumPrimes trackers.
                 public PrimesInfo call()
                 {
+                    // Maintains thread-specific numPrimes and sumPrimes trackers.
                     PrimesInfo infoP = new PrimesInfo();
 
                     int cur = counter.getAndAdd(2);
@@ -134,11 +111,12 @@ public class Primes
         // Initialize result variables on the basis that 2 is included in the range.
         int totalNumPrimes = 1;
         long totalSumPrimes = 2;
+
+        // Increment the result variables by each thread's individual results.
         for (int i = 0; i < NUM_THREADS; i++)
         {
             try 
             {
-                // Increment the total amount and sum found.
                 totalNumPrimes += tasks.get(i).get().numPrimes;
                 totalSumPrimes += tasks.get(i).get().sumPrimes;
             } 
@@ -148,15 +126,17 @@ public class Primes
             }
         }
 
-        long end = System.nanoTime();
+        long end = System.nanoTime(); // (todo) right end location?
         System.out.println((end - start) + " " + totalNumPrimes + " " + totalSumPrimes);
+
+        // Print the maximum primes found.
         while (!maxPrimes.isEmpty())
             System.out.print(maxPrimes.pollFirst() + " ");
         System.out.println();
     }
 }
 
-// Stores information about a group of primes seen by a single thread.
+// Stores information about a group of prime numbers.
 class PrimesInfo
 {
     public int numPrimes = 0;
